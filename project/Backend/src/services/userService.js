88ee8bf45098ff9a671eb1a628d13056649e35e0
nathnael <nathnaeltm17@gmail.com>
2026-04-03 16:35:24 +0300

    changes to be committed:

--- project/Backend/src/services/userService.js
+++
@@ -1,9 +0,0 @@
-const userModel = require('../models/userModel');
-
-exports.getAllUsers = async () => {
-  return await userModel.getUsers();
-};
-
-exports.createUser = async (data) => {
-  return await userModel.createUser(data);
-};
